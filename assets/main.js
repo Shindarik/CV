35102b43912dbe9088052524501a885175b8ceb5
SUSLU Matéo <78606237+Shindarik@users.noreply.github.com>
2021-03-16 15:49:42 +0100

    burger

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,18 @@
 // Var
+const burger = document.querySelector(".burger");
+const nav = document.querySelector("nav");
+const body = document.querySelector("body");
+const header = document.querySelector("header");
 
 //code
+window.addEventListener("scroll", function(){
+    header.style.transform = `translateY(${window.scrollY * 0.5}px)`;
+});
+
+burger.addEventListener("click", function(){
+
+    burger.classList.toggle("active");
+    nav.classList.toggle("active");
+    body.classList.toggle("active");
+    
+});
