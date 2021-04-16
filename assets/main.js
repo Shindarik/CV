6989ceab457731e5120fb7e89eaf7e314280676c
SUSLU Matéo <78606237+Shindarik@users.noreply.github.com>
2021-04-16 12:32:03 +0200

    u

--- assets/main.js
+++ assets/main.js
@@ -6,6 +6,7 @@ const header = document.querySelector("header");
 const textMouse = document.querySelector(".textMouse");
 const section1 = document.querySelector("#s1");
 const moiPrincipal = document.querySelector(".moiPrincipal");
+const linkNav = document.querySelectorAll(".link a");
 
 //code
 textMouse.innerHTML = textMouse.textContent.replace(/\S/g, "<span>$&</span>");
@@ -22,16 +23,20 @@ function ElementVue(e) {
     const rect = e.getBoundingClientRect();
     return (
         rect.top <= (window.innerHeight/2) &&
-        rect.bottom <= (window.innerHeight/2)
+        rect.bottom >= (window.innerHeight/2)
     );
 }
 
 window.addEventListener("scroll", function(e){
     header.style.transform = `translateY(${window.scrollY * 0.5}px)`;
     if(ElementVue(section1) == true){
-        moiPrincipal.style.opacity = 1;
+        setTimeout(() => {
+            moiPrincipal.style.opacity = 1;
+        }, 500);
+        moiPrincipal.style.transform = "translateY(-50%)";
     }else{
         moiPrincipal.style.opacity = 0;
+        moiPrincipal.style.transform = "translateY(-100%)";
     }
 });
 
@@ -47,4 +52,15 @@ header.addEventListener("mousemove", function(e){
     textMouse.style.left = (e.pageX + 10) + "px";
     textMouse.style.top = (e.pageY - window.scrollY * 0.5 - 10) + "px";
 });
+
+linkNav.forEach(e => {
+    e.addEventListener("click", function(){
+    
+        burger.classList.toggle("active");
+        nav.classList.toggle("active");
+        body.classList.toggle("active");
+        
+    });  
+});
+
 
