2217ccd4e310b717d50ee0894f0e75e2f6b58d55
SUSLU Matéo <78606237+Shindarik@users.noreply.github.com>
2021-05-06 15:29:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
         href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
     <link rel="shortcut icon" href="./images/logoCV.svg" type="image/x-icon">
+    <meta name="description" content="Voici le CV intéractif de Suslu Matéo">
     <title>CV - Suslu Matéo</title>
 </head>
 
