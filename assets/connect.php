4eea5c2ad5161df185b91609c6b579c170bac090
Shindarik <mateosuslu.pro@gmail.com>
2023-01-08 23:22:16 +0100

    Update connect.php

--- assets/connect.php
+++ assets/connect.php
@@ -1,3 +1,3 @@
 <?php
-    $link = new PDO('mysql:host=sqletud.u-pem.fr;dbname=msuslu01_db', 'msuslu01', 'Culdepoule+77', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    $link = new PDO('mysql:host=localhost:3306;dbname=mateosus_portfolio', 'mateossl', 'Culdepoule+77', array (PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 ?>
